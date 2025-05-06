dfd62929b56ce157aa5ef9cec122f82d40f2ff3c
adam0307a <104251711+adam0307a@users.noreply.github.com>
2025-05-06 21:09:44 +0300

    Update MachineDialog.tsx

--- src/components/MachineDialog.tsx
+++ src/components/MachineDialog.tsx
@@ -95,7 +95,11 @@ const MachineDialog: React.FC = () => {
       setConfirmEndOpen(false);
     }
   };
-  
+    useEffect(() => {
+    if (activeMachine && activeMachine.status !== 'available' && remainingTime <= 0) {
+      handleEndMachine();
+    }
+  }, [remainingTime, activeMachine, handleEndMachine]);
   const handleToggleExistence = () => {
     if (activeMachine) {
       toggleMachineExistence(activeMachine.id);
